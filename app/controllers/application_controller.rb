3966be17650da8b4d718053036184ca4a054c1c2
kanako.yatabe <kaaaaa.md@gmail.com>
2020-04-24 13:32:02 +0000

    submit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,9 @@ class ApplicationController < ActionController::Base
   
   def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
+    @count_likes = user.fav_microposts.count
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,32 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root to: 'toppages#index'
-    
-    get 'login', to: 'sessions#new'
-    post 'login', to: 'sessions#create'
-    delete 'logout', to: 'sessions#destroy'
-    
-    get 'signup', to: 'users#new'
-    resources :users, only: [:index, :show, :new, :create]
-    
-    resources :microposts, only: [:create, :destroy]
+  root to: 'toppages#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  
+  #フォロー中のユーザとフォローされているユーザ一覧表示
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+
+  resources :microposts, only: [:create, :destroy]
+  
+  #ログインユーザがユーザをフォロー/アンフォローできるようにするルーティング
+  resources :relationships, only: [:create, :destroy]
+  
+  #ふぁぼ一覧表示
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :likes
+    end
+  end
+  
+  #ログインユーザが投稿をfav/unfavできるようにするルーティング
+  resources :favorites, only: [:create, :destroy]
 end
