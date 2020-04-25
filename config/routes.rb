5236d74e97fe9c49385c101554155af453022831
kanako.yatabe <kaaaaa.md@gmail.com>
2020-04-25 06:19:39 +0000

    submit2

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,12 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   
-  #フォロー中のユーザとフォローされているユーザ一覧表示
+  #フォロー中のユーザとフォローされているユーザ一、お気に入り覧表示
   resources :users, only: [:index, :show, :new, :create] do
     member do
       get :followings
       get :followers
+      get :likes
     end
   end
 
@@ -20,12 +21,6 @@ Rails.application.routes.draw do
   #ログインユーザがユーザをフォロー/アンフォローできるようにするルーティング
   resources :relationships, only: [:create, :destroy]
   
-  #ふぁぼ一覧表示
-  resources :users, only: [:index, :show, :new, :create] do
-    member do
-      get :likes
-    end
-  end
   
   #ログインユーザが投稿をfav/unfavできるようにするルーティング
   resources :favorites, only: [:create, :destroy]
